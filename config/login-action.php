7e780bd8981209cf56b2c6a84e2823b2fce4c8e0
Houlaid <houlaid@isoluce.net>
2021-10-08 14:40:46 +0200

    Système de posts

---
+++ config/login-action.php
@@ -0,0 +1,43 @@
+	<?php
+	 $host = '127.0.0.1';
+	 $root = 'root';
+	 $dbLink = mysqli_connect($host, $root) or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+	 $user = $_POST['login'];
+	 $pwd = md5($_POST['mdp']);
+	 mysqli_select_db($dbLink , 'projet-web') or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
+	 $erreur = 'Mauvais id et/ou mdp.';
+	?>
+
+<?php
+	session_start();
+	$query = "SELECT * FROM User WHERE Email='$user' AND Mdp='$pwd '";
+		 if(!($dbResult = mysqli_query($dbLink, $query))){
+			 echo 'Erreur dans requête<br />'; 
+			 // Affiche le type d'erreur. echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+			 
+			 echo 'Requête : ' . $query . '<br/>';
+			 // Affiche la requête envoyée.
+			 exit();
+	}
+		else{
+			$rows = mysqli_num_rows($dbResult);
+				if($rows==1)
+				{
+					$_SESSION['login'] = $user;
+					$_SESSION['password'] = $pwd;
+					$_SESSION['suid'] = session_id();
+					header("Location: ../index.php");
+				}
+				else
+				{
+					header("Location: ../pages/login.php");
+					$_SESSION['error'] = $erreur;
+			}
+					
+	}
+	
+	
+	
+	
+		
+?>
---
+++ pages/login.php
@@ -0,0 +1,70 @@
+<?php 
+session_start();
+
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Connexion</title>
+
+    <!-- ---- CSS ---- -->
+    <link rel="stylesheet" href="../style/style.css">
+
+    <!-- ---- Script ---- -->
+    <script src="/js/jquery-3.6.0.min.js"></script>
+	<script src="/js/index.js"></script>
+	<script src="/js/validationform.js"></script>
+	
+	<!-- ---- Fonts ---- -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+</head>
+
+<header class="siteHeader">
+	<h1> 
+		Vanestarre
+	</h1>
+</header>
+
+<body>
+		<!-- ---- Formulaire de Connexion ---- -->
+		<div class id="loginForm">
+			<form class="boiteAValider" action="../config/login-action.php" method="post">
+			<?php
+                    if(isset($_SESSION["error"])){
+                        $error = $_SESSION["error"];
+                        echo "<span>$error</span>";
+                    }
+					else if(isset($_SESSION['suid']))
+					 {
+						header('Location: ../index.php');
+					 }
+                ?>  
+				<div class="form-block">
+				
+						<strong><Label for="login" class="labelID">Identifiant </Label></strong>
+							<input type="email" placeholder="Email" name="login" required> </br>
+							
+						<strong><Label for="mdp" class="labelPass">Mot de passe </Label></strong>
+							<input type="password" placeholder="Mot de passe"name="mdp" required> </br>
+						
+					<div id="Validation-Form">
+						<input type="submit" id="loginButton" name="action" value="Connexion">
+						<button id="signLink"><a href="sign.php">Inscription</a></button>
+					</div>
+				</div> 
+			</form>
+			<?php
+    unset($_SESSION["error"]);
+?>
+		</div>
+		
+		
+
+</body>
