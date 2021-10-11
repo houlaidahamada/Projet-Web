979a2af6ec8dab32ce127ec3ce5a56f74577a45c
Houlaid <houlaid@isoluce.net>
2021-10-11 14:44:52 +0200

    Faute d'orthographe

--- config/sign-action.php
+++ config/sign-action.php
@@ -3,8 +3,7 @@
  $login = '245104';
  $mysqlpass = 'marioChampi';
  $dbLink = mysqli_connect($host, $login, $mysqlpass) or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
- mysqli_select_db($dbLink, 'projetweb2_vanes')  or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
-);?>
+ mysqli_select_db($dbLink, 'projetweb2_vanes')  or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));?>
 
 
 
