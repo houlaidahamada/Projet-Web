544f3434be626850d94091a3ca5ca4dfc8baabda
Florian Rizzo <florian.rizzo@etu.univ-amu.fr>
2021-10-13 09:15:44 +0200

    ajout β

--- config/search-action.php
+++ config/search-action.php
@@ -26,7 +26,7 @@
                         echo '
                             <h1>'.$row['title'].'</h1>
                             <p>'.$row['content'].'</p>
-                            <i>'.$row['keywords'].'</i>';
+                            echo <p>β</p> . <i>'.$row['keywords'].'</i>';
                     }
                     
                     echo '</article>';
