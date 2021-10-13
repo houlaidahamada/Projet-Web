72394dfdac85a578d0733dcbafe5ed7c2f17b3af
Houlaid <houlaid@isoluce.net>
2021-10-13 08:52:44 +0200

    Lien vers index

--- pages/user.php
+++ pages/user.php
@@ -33,7 +33,7 @@ if(!isset($_SESSION['suid']))
 		<nav id="navigationMenu">
         			  <ul>
         				 <li class="logo">Vanestarre</li>
-                         <li class="items"><a href="index.php">Accueil</a></li>
+                         <li class="items"><a href="../index.php">Accueil</a></li>
         				 <li class="items"><a href="pages/user.php">Utilisateur</a></li>
         				 <li class="items"><a href="config/deconnexion.php">Deconnexion</a></li>
         				 <li class="btn"><a href="#"><i class="fas fa-bars"></i></a></li>
