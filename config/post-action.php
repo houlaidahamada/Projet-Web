a500b68804e40b9c0734f901d986f4cb40991e56
BuildTools <unconfigured@null.spigotmc.org>
2021-10-14 22:49:41 +0200

    Implémentation images de posts

--- config/post-action.php
+++ config/post-action.php
@@ -33,11 +33,12 @@
 
 				 if (in_array($file_ext_lc, $allowed_ext)){
 					 $new_img_name = uniqid("IMG-", true).'.'.$file_ext_lc;
-					 $img_upload_path = 'images/'.$new_img_name;
+					 $img_upload_path = '../images/'.$new_img_name;
 					 move_uploaded_file($tmp_name, $img_upload_path);
 
 					 $sql = "INSERT INTO posts(image_url) VALUES('$new_img_name')";
 					 mysqli_query($dbLink, $sql);
+					 header("Location: ../index.php");
 
 				 }
 				 else
